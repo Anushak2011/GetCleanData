cf384a5d0ce868cae730045f09a7e6ba6648d229
Anushak2011 <anushak@ucla.edu>
2015-08-23 15:25:48 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -27,34 +27,28 @@ subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 names(X_test) = features
 
 X_test = X_test[,extract_features] #extract mean and S.D. for each measurement
-
 y_test[,2] = activity_labels[y_test[,1]]
 names(y_test) = c("Activity_ID", "Activity_Label") #activity labels
 names(subject_test) = "subject"
 
 test_data <- cbind(as.data.table(subject_test), y_test, X_test) #bind data
-
 X_train <- read.table("./UCI HAR Dataset/train/X_train.txt") #load and process X_train
 y_train <- read.table("./UCI HAR Dataset/train/y_train.txt") #load and process Y_train
 
 subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
-
 names(X_train) = features
  
 X_train = X_train[,extract_features] #extract mean and S.D. for each measurement
-
 y_train[,2] = activity_labels[y_train[,1]]
 names(y_train) = c("Activity_ID", "Activity_Label") #activity data
 names(subject_train) = "subject"
 
 train_data <- cbind(as.data.table(subject_train), y_train, X_train) #bind data
-
 data = rbind(test_data, train_data) #merge test data and train data into one set
 
 id_labels   = c("subject", "Activity_ID", "Activity_Label")
 data_labels = setdiff(colnames(data), id_labels)
 melt_data      = melt(data, id = id_labels, measure.vars = data_labels)
-
 tidy_data   = dcast(melt_data, subject + Activity_Label ~ variable, mean) #use dcast to implement mean function to set of data
 
 write.table(tidy_data, file = "./tidy_data.txt") #write_table for tidy_data.txt to complete program requirements
