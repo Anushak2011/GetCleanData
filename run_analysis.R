edf4b523f5657566ef583b25a5c2cb8f575ace6d
Anushak2011 <anushak@ucla.edu>
2015-08-23 15:24:51 -0700

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,7 @@
+## Instructions for submission:
+##Please upload the tidy data set created in step 5 of the instructions. 
+## Please upload your data set as a txt file created with write.table() using row.name=FALSE (do not cut and paste a dataset directly into the text box, as this may cause errors saving your submission).
+##----------------------------------------------------------------------------------------
 ## Create one R script called run_analysis.R that does the following:
 ## 1. Merges the training and the test sets to create one data set.
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
